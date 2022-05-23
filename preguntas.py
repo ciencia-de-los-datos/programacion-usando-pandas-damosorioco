8407d3007f5c487b44abd3b8854f31cf7b5250b0
damosorioco <101427484+damosorioco@users.noreply.github.com>
2022-05-22 21:38:15 -0500

    Update preguntas.py
    p1

--- preguntas.py
+++ preguntas.py
@@ -22,7 +22,17 @@ def pregunta_01():
     40
 
     """
-    return
+    import pandas as pd
+    
+    tbl0 = pd.read_csv("tbl0.tsv", sep="\t")
+    tbl1 = pd.read_csv("tbl1.tsv", sep="\t")
+    tbl2 = pd.read_csv("tbl2.tsv", sep="\t")
+    
+    tb10.count()
+    
+    
+    
+    return tb10.count()
 
 
 def pregunta_02():
